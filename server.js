3655852c7b23f300e74471d199fd3b46269286d5
mayankvpunjabi-wq <mayankvpunjabi@gmail.com>
2025-09-20 17:20:12 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,22 +6,20 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Needed for ES Modules to get __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, 'public'))); // Make sure 'public' is lowercase
+app.use(express.static(path.join(__dirname, 'public'))); // make sure folder is lowercase
 
 // Your other routes here
-// Example route
 app.get('/hello', (req, res) => {
   res.send('Hello world!');
 });
 
-// Catch-all route to serve index.html
-app.get('*', (req, res) => {
+// Correct catch-all for Express v5+
+app.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
